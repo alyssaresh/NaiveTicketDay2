c0d24227e6b3f7c2a740f22245ed259f0ee79e7c
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-10-24 17:44:52 -0400

    unfinished, added change method and amount owed method

--- src/main/java/TicketMachine.java
+++ src/main/java/TicketMachine.java
@@ -18,6 +18,8 @@ public class TicketMachine {
     private Integer total;
     // The number of tickets printed.
     private Integer ticketNumber;
+    private Integer change;
+    private Integer amountOwed;
 
     /**
      * Create a machine that issues tickets of the given price.
@@ -30,6 +32,8 @@ public class TicketMachine {
         balance = 0;
         total = 0;
         ticketNumber = 0;
+        change = 0;
+        amountOwed = 0;
     }
 
     /**
@@ -89,32 +93,46 @@ public class TicketMachine {
         return "Ticket price: " + price + " cents. " + "Your total is " + total + ".";
     }
 
+    public String getChange() {
+        if (balance > price) {
+            change = balance - price;
+            return "Your change is " + change;
+        } else {
+            amountOwed = price - balance;
+            return "You owe " + amountOwed;
+        }
+    }
+
         public static void main(String[] args) {
         TicketMachine ticketMachine1 = new TicketMachine(13);
         //get ticket price
-        System.out.println(ticketMachine1.getPrice());
+        System.out.println("Ticket price: " + ticketMachine1.getPrice());
         //insert 5 dollars
-        ticketMachine1.insertMoney(10);
+        ticketMachine1.insertMoney(15);
         //check balance
-        System.out.println(ticketMachine1.getBalance());
+        System.out.println("Your balance is: " + ticketMachine1.getBalance());
+        System.out.println(ticketMachine1.getChange());
+        //asking for more money and giving change is working, but only before printing the ticket. Possibly because balance goes down to zero when ticket prints.
+
         /*
         add more money and check balance
         ticketMachine1.insertMoney(5);
-        System.out.println(ticketMachine1.getBalance());
+        System.out.println("You have entered: " + ticketMachine1.getBalance());
         */
         System.out.println(ticketMachine1.printTicket());
         //see new balance after ticket prints
         System.out.println(ticketMachine1.getBalance());
-
+/*
         TicketMachine ticketMachine2 = new TicketMachine(20);
-        System.out.println(ticketMachine2.getPrice());
+        System.out.println("Ticket price: " + ticketMachine2.getPrice());
         ticketMachine2.insertMoney(10);
         System.out.println(ticketMachine2.getBalance());
         System.out.println(ticketMachine2.printTicket());
         System.out.println(ticketMachine2.getBalance());
+ */
     }
 
 }
 
 //could you use an if statement to get change/not print a ticket?
-
+//need to fix change function (make an if statement
